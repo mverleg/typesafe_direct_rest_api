3b42ab381c16d1e1f1e90a5504dd9009c82150ae
Mark <mverleg.noreply@gmail.com>
2018-07-22 16:45:14 +0200

    Add tests for response and do cleanup

--- apilib/apilib/response.rs
+++ apilib/apilib/response.rs
@@ -5,7 +5,7 @@ use serde::Deserialize;
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum TResponse<T: Transfer> {
     Ok(T),
-    Err(String),
+    Err(u16, String),
 }
 
 impl<'de, T: Transfer + Serialize + Deserialize<'de>> TResponse<T> {
@@ -15,19 +15,11 @@ impl<'de, T: Transfer + Serialize + Deserialize<'de>> TResponse<T> {
 
     // maybe 'code' should be an enum to only allow valid http codes
     pub fn err(code: u16, message: String) -> Self {
-        TResponse::Err(message)
+        TResponse::Err(code, message)
     }
 }
 
 impl<'de, T: Transfer + Serialize + Deserialize<'de>> Transfer for TResponse<T> {
-//    fn encode(self) -> String {
-//        serde_json::to_string(&self).unwrap()
-//    }
-//
-//    fn decode(repr: &str) -> Self {
-//        serde_json::from_str(repr).unwrap()
-//    }
-
     fn clean(self) -> Self {
         match self {
             TResponse::Ok(value) => TResponse::Ok(value.clean()),
@@ -41,9 +33,9 @@ impl<T> PartialEq for TResponse<T> where T: Transfer + PartialEq {
     fn eq(&self, other: &TResponse<T>) -> bool {
         match (self, other) {
             (TResponse::Ok(sok), TResponse::Ok(ook)) => sok == ook,
-            (TResponse::Ok(_), TResponse::Err(_)) => false,
-            (TResponse::Err(_), TResponse::Ok(_)) => false,
-            (TResponse::Err(ser), TResponse::Err(oer)) => ser == oer,
+            (TResponse::Ok(_), TResponse::Err(_, _)) => false,
+            (TResponse::Err(_, _), TResponse::Ok(_)) => false,
+            (TResponse::Err(sec, ser), TResponse::Err(oec, oer)) => sec == oec && ser == oer,
         }
     }
 }
